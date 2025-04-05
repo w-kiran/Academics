45ae690161d0163da2cccc1d34bd9ca89aa2b43d
Kiran Joshi Sukubhattu <158054057+w-kiran@users.noreply.github.com>
2025-04-05 09:05:32 +0545

    BScCSIT

---
+++ BSc. CSIT/CSIT 5th sem/Design and Analysis of Algorithm/Lab/code/1_1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int count = 0;
+void BubbleSort(int A[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(A[j]>A[j+1])
+            {
+                int t= A[j];
+                A[j]=A[j+1];
+                A[j+1]=t;
+                
+            }
+            
+            count =count+8;
+            
+        }
+        count = count+4;
+    }    
+}
+int main()
+{
+cout<<"Kiran Joshi Sukubhattu\n";
+int a[9] = {100,222,2,45,89,150,170,10,300};
+//int a[9] = {1,2,3,4,5,6,7,8,9};
+
+cout<<"Before sorting: \n";
+   for(int i=0 ;i<9 ;i++)
+        {
+    cout<<a[i]<<"\t" ;
+    
+    
+        }
+        
+    cout<<endl;
+    
+BubbleSort(a,9);
+cout<<"After sorting: \n";
+ for(int i=0 ;i<9 ;i++)
+        {
+    cout<<a[i]<<"\t" ;
+        }
+cout<<endl;        
+cout<<"No. of Steps required : "<<count;        
+        
+return 0;
+}
+
