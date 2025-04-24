a1c37dfd9862a004f8b30ca49ccaa04dedbe7fd0
Kiran Joshi Sukubhattu <158054057+w-kiran@users.noreply.github.com>
2025-04-24 18:19:01 +0545

    BScCSIT

---
+++ BSc. CSIT/CSIT 5th sem/WebTechnology/Lab/codes/Lab4/2.php
@@ -0,0 +1,10 @@
+<?php
+$integerVar = 45;
+$floatVar = 3.14;
+$stringVar = "Kiran, Code!";
+define("MY_CONSTANT", "This is a constant");
+echo MY_CONSTANT . "<br>";
+echo $integerVar . "<br>";
+echo $floatVar . "<br>";
+echo $stringVar;
+?>
---
+++ BSc. CSIT/CSIT 5th sem/WebTechnology/Lab/codes/Lab4/9.php
@@ -0,0 +1,6 @@
+<?php
+setcookie("user_pref", "dark_mode", time() + 3600, "/");
+if (isset($_COOKIE["user_pref"])) {
+ echo "User preference: " . $_COOKIE["user_pref"];
+}
+?>
